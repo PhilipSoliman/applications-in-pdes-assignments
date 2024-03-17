fixing fd jacobian

code/classes/ebm.py
@@ -10,6 +10,7 @@ from numpy.polynomial.legendre import leggauss
 
 from non_linear_system import NonLinearSystem
 
+
 class EBM(NonLinearSystem):
     # problem constants and parameters (capitalized is default value)
     mu_default = 30.0  # greenhouse gas and particle parameter
@@ -42,9 +43,7 @@ class EBM(NonLinearSystem):
 
         # default values of properties
         self._T_coeffs = np.zeros(n_polys)
-        self._T_coeffs[0] = (
-            EBM.T_star
-        )  # earth has a homogeneous temperature
+        self._T_coeffs[0] = EBM.T_star  # earth has a homogeneous temperature
         self._mu = EBM.mu_default
         self._D = EBM.D_default
 
@@ -141,15 +140,16 @@ class EBM(NonLinearSystem):
         --------
         np.ndarray: matrix such that out[i,j] = (F(T(a_i + h))[j] - F(T)[j]) / h
         """
-        F_T = self.evaluate()
-        T_coeffs_plus_h = self.T_coeffs * np.ones((self.n_polys, self.n_polys)) + h * np.eye(self.n_polys)
+        T_coeffs_plus_h = self.T_coeffs * np.ones(
+            (self.n_polys, self.n_polys)
+        ) + h * np.eye(self.n_polys)
         T_plus_h = T_coeffs_plus_h @ self.legendre_polys
-        integrand = self.energy_balance(T_plus_h) # n x n_samples
+        integrand = (
+            self.energy_balance(T_plus_h) - self.energy_balance(self.T_eval())
+        ) / h
         test_function = self.legendre_polys
-        weak_form = np.einsum("is,js->ijs", integrand, test_function)
-        F_T_plus_h = weak_form @ self.quad_weights
-        return (F_T_plus_h - F_T[:, None]) / h
-    
+        return np.einsum("is,js->jis", integrand, test_function) @ self.quad_weights
+
     def update_solution(self, update: np.ndarray) -> None:
         """
         Add the given update to current solution.
@@ -206,11 +206,7 @@ class EBM(NonLinearSystem):
 
     @staticmethod
     def dalbedo(T: np.ndarray) -> np.ndarray:
-        return (
-            EBM.M
-            * (EBM.a_2 - EBM.a_1)
-            / (2 * np.cos(EBM.M * (T - EBM.T_star)) ** 2)
-        )
+        return EBM.M * (EBM.a_2 - EBM.a_1) / (2 * np.cos(EBM.M * (T - EBM.T_star)) ** 2)
 
     @staticmethod
     # Black body radiation (R_E)

code/ebm_experiments.py
@@ -42,7 +42,7 @@ plt.savefig(fn, dpi=500)
 print("Saved initial guess for T to:\n  ", fn)
 
 ################### Newton-Raphson Method ####################
-rootfinding = RootFinding(maxiter=1)
+rootfinding = RootFinding(maxiter=maxiter)
 
 # run Newton-Raphson method
 errors = rootfinding.newtonRaphson(ebm)
@@ -51,7 +51,7 @@ T_final_exact = ebm.T_x(ebm.x)
 # run Newton-Raphson method with finite difference
 ebm.T_coeffs = np.zeros(n)
 ebm.T_coeffs[0] = initial_temperature
-errors_fd = rootfinding.newtonRaphson(ebm, exact=False, stepsize=1e-3)
+errors_fd = rootfinding.newtonRaphson(ebm, exact=False, stepsize=1e3)
 T_final_fd = ebm.T_x(ebm.x)
 
 # plot error convergence
@@ -75,3 +75,27 @@ plt.legend()
 fn = output_dir / "equilibrium_T.png"
 plt.savefig(fn, dpi=500)
 print("Saved equilibrium T to:\n  ", fn)
+
+##################### exact vs finite difference jacobian #####################
+stepsizes = np.logspace(-13, 7, 100)
+errors = []
+ebm.T_coeffs = np.zeros(n)
+ebm.T_coeffs[0] = initial_temperature
+_ = rootfinding.newtonRaphson(ebm) # find equilibrium solution
+jacobian_exact = ebm.evaluate_derivative()
+print("Running finite difference jacobian approximation analysis...")
+for h in stepsizes:
+    jacobian_fd = ebm.evaluate_derivative_finite_difference(h)
+    error = np.linalg.norm(jacobian_exact-jacobian_fd, ord='fro')
+    errors.append(error)
+
+plt.close()
+plt.plot(stepsizes, errors)
+plt.xscale("log")
+plt.yscale("log")
+plt.title("Error in Jacobian approximation")
+plt.xlabel("Stepsize")
+plt.ylabel("Error")
+fn = output_dir / "jacobian_approximation_error.png"
+plt.savefig(fn, dpi=500)
+print("Saved jacobian approximation error to:\n  ", fn)