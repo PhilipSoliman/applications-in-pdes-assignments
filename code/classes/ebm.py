FIXED NUMERICAL JACOBIAN

code/classes/ebm.py
@@ -140,13 +140,12 @@ class EBM(NonLinearSystem):
         --------
         np.ndarray: matrix such that out[i,j] = (F(T(a_i + h))[j] - F(T)[j]) / h
         """
-        T_coeffs_plus_h = self.T_coeffs[:,None] * np.ones(
-            (self.n_polys, self.n_polys)
-        ) + h * np.eye(self.n_polys)
+        T_coeffs_plus_h = self.T_coeffs + h * np.eye(self.n_polys)
         T_plus_h = T_coeffs_plus_h @ self.legendre_polys
         integrand = (
-            self.energy_balance(T_plus_h) - self.energy_balance(self.T_eval())
-        ) / h
+            self.energy_balance(T_plus_h, T_coeffs_plus_h)
+            - self.energy_balance(self.T_eval())
+        ) / h  # [EBM(x, T(a + e_i*h)) - EBM(x, T)] / h
         test_function = self.legendre_polys
         return np.einsum("is,js->ijs", integrand, test_function) @ self.quad_weights
 
@@ -157,8 +156,8 @@ class EBM(NonLinearSystem):
         self.T_coeffs += update
 
     ######## Energy balance terms ########
-    def energy_balance(self, T: np.ndarray) -> np.ndarray:
-        return self.R_D() + self.R_A(self.quad_samples, T) - self.R_E(T)
+    def energy_balance(self, T: np.ndarray, T_coeffs: np.ndarray = None) -> np.ndarray:
+        return self.R_D(T_coeffs) + self.R_A(self.quad_samples, T) - self.R_E(T)
 
     def energy_balance_derivative(self, T: np.ndarray) -> np.ndarray:
         """
@@ -182,17 +181,11 @@ class EBM(NonLinearSystem):
 
     # solar radiation (R_A)
     def R_A(self, x: np.ndarray, T: np.ndarray) -> np.ndarray:
-        # return EBM.Q_solar(x) * (1 - EBM.albedo(T)) + self.mu
-        out = EBM.Q_solar(x) * (1 - EBM.albedo(T)) + self.mu
-        # return np.zeros_like(out)
-        return out
+        return EBM.Q_solar(x) * (1 - EBM.albedo(T)) + self.mu
 
     @staticmethod
     def dR_A(x: np.ndarray, T: np.ndarray) -> np.ndarray:
-        # return EBM.Q_solar(x) * EBM.dalbedo(T)
-        out = EBM.Q_solar(x) * EBM.dalbedo(T)
-        # return np.zeros_like(out)
-        return out
+        return EBM.Q_solar(x) * EBM.dalbedo(T)
 
     @staticmethod
     def Q_solar(x: np.ndarray) -> np.ndarray:
@@ -208,23 +201,17 @@ class EBM(NonLinearSystem):
     def dalbedo(T: np.ndarray) -> np.ndarray:
         return EBM.M * (EBM.a_2 - EBM.a_1) / (2 * np.cos(EBM.M * (T - EBM.T_star)) ** 2)
 
-    @staticmethod
     # Black body radiation (R_E)
+    @staticmethod
     def R_E(T: np.ndarray) -> np.ndarray:
-        # return EBM.epsilon_0 * EBM.sigma_0 * T**4
-        out = EBM.epsilon_0 * EBM.sigma_0 * T**4
-        # return np.zeros_like(out)
-        return out
+        return EBM.epsilon_0 * EBM.sigma_0 * T**4
 
     @staticmethod
     def dR_E(T: np.ndarray) -> np.ndarray:
-        # return 4 * EBM.epsilon_0 * EBM.sigma_0 * T**3
-        out = 4 * EBM.epsilon_0 * EBM.sigma_0 * T**3
-        # return np.zeros_like(out)
-        return out
+        return 4 * EBM.epsilon_0 * EBM.sigma_0 * T**3
 
     # dispersion & legendre polynomials
-    def R_D(self) -> np.ndarray:
+    def R_D(self, T_coeffs: np.ndarray = None) -> np.ndarray:
         """
         Evaluate the dispersion term using the coefficients and eigenvalues
         of the respective Legendre polynomials.
@@ -234,7 +221,9 @@ class EBM(NonLinearSystem):
         np.ndarray (m,): The dispersion term evaluated for the current T_coeffs
             and at the m quad_sample points.
         """
-        return (self.legendre_eigs / self.D * self.T_coeffs) @ self.legendre_polys
+        if T_coeffs is None:
+            T_coeffs = self.T_coeffs
+        return (self.legendre_eigs / self.D * T_coeffs) @ self.legendre_polys
 
     def dR_D(self) -> np.ndarray:
         """

code/classes/root_finding.py
@@ -8,6 +8,7 @@ class RootFinding:
     def __init__(self, tolerance: float = 1e-5, maxiter: int = 100) -> None:
         self.tolerance = tolerance
         self.maxiter = maxiter
+        self.converged = False
 
     def newtonRaphson(
         self, NLS: NonLinearSystem, exact: bool = True, stepsize: float = 1e-6
@@ -38,7 +39,7 @@ class RootFinding:
                 dF = NLS.evaluate_derivative()
             else:
                 dF = NLS.evaluate_derivative_finite_difference(stepsize)
-            print(dF)
+            # print(dF)
             update = np.linalg.solve(dF, F)
             NLS.update_solution(-update)
 
@@ -51,9 +52,13 @@ class RootFinding:
 
         if error <= self.tolerance:
             pbar.close()
+            self.converged = True
             print("Newton-Raphson converged.")
 
         return errors
+    
+    def broydensMethod() -> None:
+        pass
 
     def arcLength() -> None:
         pass

code/ebm_experiments.py
@@ -16,7 +16,7 @@ print(f"Output directory:\n  {output_dir}")
 pyutils.set_style()
 
 ############### Computation parameters ####################
-n = 10  # number of Legendre polynomials
+n = 5  # number of Legendre polynomials
 number_of_quad_points = 2 * n  # number of quadrature points
 grid_resolution = 100  # resolution of the grid
 initial_temperature = (
@@ -42,26 +42,43 @@ plt.savefig(fn, dpi=500)
 print("Saved initial guess for T to:\n  ", fn)
 
 ################### Newton-Raphson Method ####################
-rootfinding = RootFinding(maxiter=1)
+rootfinding = RootFinding(maxiter=1000)
 
 # run Newton-Raphson method
 errors = rootfinding.newtonRaphson(ebm)
 T_final_exact = ebm.T_x(ebm.x)
+conv_exact = rootfinding.converged
 
 # run Newton-Raphson method with finite difference
 ebm.T_coeffs = np.zeros(n)
 ebm.T_coeffs[0] = initial_temperature
 errors_fd = rootfinding.newtonRaphson(ebm, exact=False, stepsize=1e3)
 T_final_fd = ebm.T_x(ebm.x)
+conv_fd = rootfinding.converged
 
 # plot error convergence
 plt.close()
-plt.plot(errors)
-plt.plot(errors_fd)
+plt.plot(errors, label="Exact Jacobian")
+if conv_exact: 
+    marker = 'rx'
+    label = "convergence" 
+else: 
+    marker = 'ro'
+    label = "no convergence"
+plt.plot(len(errors), errors[-1], marker, label=label)
+plt.plot(errors_fd, label="Finite Difference Jacobian")
+if conv_fd: 
+    marker = 'rx'
+    label = "convergence" 
+else: 
+    marker = 'ro'
+    label = "no convergence"
+plt.plot(len(errors_fd), errors_fd[-1], marker, label=label)
 plt.title("Error convergence")
 plt.xlabel("Iteration")
 plt.ylabel("Error")
-plt.yscale("log")
+# plt.yscale("log")
+plt.legend()
 fn = output_dir / "error_convergence.png"
 plt.savefig(fn, dpi=500)
 print("Saved error convergence to:\n  ", fn)
@@ -81,12 +98,12 @@ stepsizes = np.logspace(-3, 7, 100)
 errors = []
 ebm.T_coeffs = np.zeros(n)
 ebm.T_coeffs[0] = initial_temperature
-_ = rootfinding.newtonRaphson(ebm) # find equilibrium solution
+_ = rootfinding.newtonRaphson(ebm)  # find equilibrium solution
 jacobian_exact = ebm.evaluate_derivative()
 print("Running finite difference jacobian approximation analysis...")
 for h in stepsizes:
     jacobian_fd = ebm.evaluate_derivative_finite_difference(h)
-    error = np.linalg.norm(jacobian_exact-jacobian_fd, ord='fro')
+    error = np.linalg.norm(jacobian_exact - jacobian_fd, ord=2)
     errors.append(error)
 
 plt.close()