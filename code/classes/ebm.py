fixed incorrect exact jacobian (derivative of radiation term) + updated figs

code/classes/ebm.py
@@ -120,32 +120,40 @@ class EBM(NonLinearSystem):
 
     def evaluate_derivative(self) -> np.ndarray:
         """
-
         Returns:
         --------
-        np.ndarray: matrix such that out[i,j] = int_{-1}{1} [(lambda_j + dR_A(T) - dR_E(T)) * phi_j] * phi_i dx
+        np.ndarray: matrix such that out[i,j] = int_{-1}{1} [(lambda_j + dR_A(T) - dR_E(T))] * phi_j * phi_i dx
         """
         T = self.T_eval()
         integrand = self.energy_balance_derivative(T)
         test_function = self.legendre_polys
-        weak_form_derivative = np.einsum("js,is->ijs", integrand, test_function)
-        # return np.einsum("ijs,s->ij", weak_form_derivative, self.quad_weights)
+        weak_form_derivative = np.einsum("is,js->ijs", integrand, test_function)
         return weak_form_derivative @ self.quad_weights
 
-    def evaluate_derivative_finite_difference(self, h: float = 1e-6) -> np.ndarray:
+    def evaluate_derivative_finite_difference(
+        self, h: float = 1e-6, order: int = -1
+    ) -> np.ndarray:
         """
-        Evaluate the derivative of the non-linear system of equations using finite difference.
+        Evaluate the derivative of the non-linear system of equations using finite differences.
+
+        Args:
+        h (float); specifies step size for finite difference
+        !!!NI order (int); specifies the order of the finite difference method.
+            2 -> central difference
+            1 -> forward difference
+            -1 -> backward difference
 
         Returns:
         --------
         np.ndarray: matrix such that out[i,j] = (F(T(a_i + h))[j] - F(T)[j]) / h
         """
+        # TODO: implement other finite difference methods (backward, central difference, etc...)
         T_coeffs_plus_h = self.T_coeffs + h * np.eye(self.n_polys)
         T_plus_h = T_coeffs_plus_h @ self.legendre_polys
         integrand = (
             self.energy_balance(T_plus_h, T_coeffs_plus_h)
             - self.energy_balance(self.T_eval())
-        ) / h  # [EBM(x, T(a + e_i*h)) - EBM(x, T)] / h
+        ) / h  # [EBM(x, T(a + e_j*h)) - EBM(x, T)] / h
         test_function = self.legendre_polys
         return np.einsum("is,js->ijs", integrand, test_function) @ self.quad_weights
 
@@ -174,10 +182,11 @@ class EBM(NonLinearSystem):
         np.ndarray (n, m): The derivative of the energy balance terms evaluated at the given coefficients.
             m is the number of sample points and n is the number of Legendre polynomials.
         """
-        return (
-            self.dR_D()
-            + (self.dR_A(self.quad_samples, T) - self.dR_E(T)) * self.legendre_polys
-        )
+        diffusion_term = self.dR_D()
+        flux_terms = (
+            self.dR_A(self.quad_samples, T) - self.dR_E(T)
+        ) * self.legendre_polys
+        return diffusion_term + flux_terms
 
     # solar radiation (R_A)
     def R_A(self, x: np.ndarray, T: np.ndarray) -> np.ndarray:
@@ -185,7 +194,7 @@ class EBM(NonLinearSystem):
 
     @staticmethod
     def dR_A(x: np.ndarray, T: np.ndarray) -> np.ndarray:
-        return EBM.Q_solar(x) * EBM.dalbedo(T)
+        return -EBM.Q_solar(x) * EBM.dalbedo(T)
 
     @staticmethod
     def Q_solar(x: np.ndarray) -> np.ndarray:
@@ -199,7 +208,9 @@ class EBM(NonLinearSystem):
 
     @staticmethod
     def dalbedo(T: np.ndarray) -> np.ndarray:
-        return EBM.M * (EBM.a_2 - EBM.a_1) / (2 * np.cos(EBM.M * (T - EBM.T_star)) ** 2)
+        return (
+            EBM.M * (EBM.a_2 - EBM.a_1) / (2 * np.cosh(EBM.M * (T - EBM.T_star)) ** 2)
+        )
 
     # Black body radiation (R_E)
     @staticmethod
@@ -235,4 +246,4 @@ class EBM(NonLinearSystem):
         np.ndarray (n, m): The derivative of the dispersion term evaluated at the given coefficients.
             m is the number of sample points and n is the number of Legendre polynomials.
         """
-        return self.legendre_eigs[:, None] / self.D * self.legendre_polys
+        return (self.legendre_eigs[:, None] / self.D) * self.legendre_polys

code/classes/root_finding.py
@@ -9,27 +9,28 @@ class RootFinding:
         self.tolerance = tolerance
         self.maxiter = maxiter
         self.converged = False
+        self.output = False
 
     def newtonRaphson(
         self, NLS: NonLinearSystem, exact: bool = True, stepsize: float = 1e-6
     ) -> list:
         error = np.linalg.norm(NLS.evaluate())
-        errors = [error]
+        errors = []
         i = 0
         if exact:
-            print(f"Running Newton-Raphson with exact derivatives...")
+            self.print(f"Running Newton-Raphson with exact derivatives...")
         else:
-            print(
+            self.print(
                 f"Running Newton-Raphson with finite difference derivatives (h = {stepsize})..."
             )
         pbar = tqdm(desc="", total=self.maxiter, unit="NR update", miniters=5)
         while error > self.tolerance:
             pbar.set_description(
-                f"iteration: {i}, error: {errors[i]:.2e}, tolerance: {self.tolerance:.2e}, maxiter: {self.maxiter}"
+                f"iteration: {i}, error: {error:.2e}, tolerance: {self.tolerance:.2e}, maxiter: {self.maxiter}"
             )
             if i == self.maxiter:
                 pbar.close()
-                print(
+                self.print(
                     "Newton-Raphson method did not converge within the maximum number of iterations. Exiting..."
                 )
                 break
@@ -53,12 +54,16 @@ class RootFinding:
         if error <= self.tolerance:
             pbar.close()
             self.converged = True
-            print("Newton-Raphson converged.")
+            self.print("Newton-Raphson converged.")
 
         return errors
-    
+
     def broydensMethod() -> None:
         pass
 
     def arcLength() -> None:
         pass
+
+    def print(self, message: str) -> None:
+        if self.output:
+            print(message)

code/ebm_experiments.py
@@ -43,7 +43,7 @@ print("Saved initial guess for T to:\n  ", fn)
 
 ################### Newton-Raphson Method ####################
 rootfinding = RootFinding(maxiter=1000)
-
+rootfinding.output = True
 # run Newton-Raphson method
 errors = rootfinding.newtonRaphson(ebm)
 T_final_exact = ebm.T_x(ebm.x)
@@ -52,31 +52,51 @@ conv_exact = rootfinding.converged
 # run Newton-Raphson method with finite difference
 ebm.T_coeffs = np.zeros(n)
 ebm.T_coeffs[0] = initial_temperature
-errors_fd = rootfinding.newtonRaphson(ebm, exact=False, stepsize=1e3)
+errors_fd = rootfinding.newtonRaphson(ebm, exact=False, stepsize=1e-2)
 T_final_fd = ebm.T_x(ebm.x)
 conv_fd = rootfinding.converged
 
 # plot error convergence
 plt.close()
-plt.plot(errors, label="Exact Jacobian")
-if conv_exact: 
-    marker = 'rx'
-    label = "convergence" 
-else: 
-    marker = 'ro'
+plt.plot(np.arange(len(errors)), errors, label="Exact Jacobian", color="blue")
+if conv_exact:
+    marker = "x"
+    label = "convergence"
+else:
+    marker = "o"
     label = "no convergence"
-plt.plot(len(errors), errors[-1], marker, label=label)
-plt.plot(errors_fd, label="Finite Difference Jacobian")
-if conv_fd: 
-    marker = 'rx'
-    label = "convergence" 
-else: 
-    marker = 'ro'
+plt.plot(
+    len(errors) - 1,
+    errors[-1],
+    linestyle="None",
+    marker=marker,
+    label=label,
+    color="blue",
+)
+plt.plot(
+    np.arange(len(errors_fd)),
+    errors_fd,
+    label="Finite Difference Jacobian",
+    linestyle="--",
+    color="orange",
+)
+if conv_fd:
+    marker = "x"
+    label = "convergence"
+else:
+    marker = "o"
     label = "no convergence"
-plt.plot(len(errors_fd), errors_fd[-1], marker, label=label)
-plt.title("Error convergence")
+plt.plot(
+    len(errors_fd) - 1,
+    errors_fd[-1],
+    linestyle="None",
+    marker=marker,
+    label=label,
+    color="orange",
+)
+plt.title("Norm of non-linear system")
 plt.xlabel("Iteration")
-plt.ylabel("Error")
+plt.ylabel("$||F(T_k)||_2$")
 # plt.yscale("log")
 plt.legend()
 fn = output_dir / "error_convergence.png"
@@ -85,8 +105,14 @@ print("Saved error convergence to:\n  ", fn)
 
 # plot final solution
 plt.close()
-plt.plot(ebm.x, T_final_exact, label="Exact Jacobian")
-plt.plot(ebm.x, T_final_fd, label="Finite Difference Jacobian")
+plt.plot(ebm.x, T_final_exact, label="Exact Jacobian", color="blue")
+plt.plot(
+    ebm.x,
+    T_final_fd,
+    label="Finite Difference Jacobian",
+    linestyle="--",
+    color="orange",
+)
 plt.title("Final solution for T")
 plt.legend()
 fn = output_dir / "equilibrium_T.png"
@@ -101,18 +127,36 @@ ebm.T_coeffs[0] = initial_temperature
 _ = rootfinding.newtonRaphson(ebm)  # find equilibrium solution
 jacobian_exact = ebm.evaluate_derivative()
 print("Running finite difference jacobian approximation analysis...")
+jacobian_exact_nrm = np.linalg.norm(jacobian_exact, ord=2)
 for h in stepsizes:
     jacobian_fd = ebm.evaluate_derivative_finite_difference(h)
-    error = np.linalg.norm(jacobian_exact - jacobian_fd, ord=2)
+    error = np.linalg.norm(jacobian_exact - jacobian_fd, ord=2) / jacobian_exact_nrm
     errors.append(error)
 
 plt.close()
-plt.plot(stepsizes, errors)
-plt.xscale("log")
-plt.yscale("log")
-plt.title("Error in Jacobian approximation")
-plt.xlabel("Stepsize")
-plt.ylabel("Error")
+fig, ax = plt.subplots(layout="tight")
+ax.plot(stepsizes, errors)
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_title("Error in Jacobian approximation")
+ax.set_xlabel("Stepsize")
+ax.set_ylabel("$||J_{exact} - J_{FD}||_2 / ||J_{exact}||_2$")
+ax.tick_params(axis="y", labelcolor="blue")
+
+# check convergence of Newton-Raphson method using FD jacobian vs stepsize
+num_iterations = np.zeros_like(stepsizes)
+rootfinding.output = False
+for i, h in enumerate(stepsizes):
+    ebm.T_coeffs = np.zeros(n)
+    ebm.T_coeffs[0] = initial_temperature
+    errors = rootfinding.newtonRaphson(ebm, exact=False, stepsize=h)
+    num_iterations[i] = len(errors)
+
+secax_y = ax.twinx()
+secax_y.plot(stepsizes, num_iterations, color="red")
+secax_y.set_ylabel("Number of iterations")
+secax_y.tick_params(axis="y", labelcolor="red")
+
 fn = output_dir / "jacobian_approximation_error.png"
 plt.savefig(fn, dpi=500)
 print("Saved jacobian approximation error to:\n  ", fn)