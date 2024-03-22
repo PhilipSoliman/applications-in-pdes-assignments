18b640fdc8dd36a1777684322f0ec0bd33cb68e1
Philip Soliman <philipsoliman4133@gmail.com>
2024-03-23 00:37:30 +0100

    finished code

--- code/ebm_continuation.py
+++ code/ebm_continuation.py
@@ -45,25 +45,25 @@ print(f"Continuation solution:\n\tcoeffs = {ebm.T_coeffs}\n\taverage = {ebm.T_av
 ############### Continuation (loop) ##########################
 print("Building bifurcation diagram (continuation loop)...")
 D_values = [0.003, 0.3, 30]
-# stepsizes = [0.1, 0.1, 0.1]
-# maxContinuations_l = [1000, 1000, 1000]
-# tuning_factors = [0.00001, 0.00001, 0.00001]
+stepsizes = [0.1, 0.1, 0.01]
+maxContinuations_l = [1000, 1000, 10000]
+tuning_factors = [0.00001, 0.00001, 0.00001]
 fig, axs = plt.subplots(
     nrows=1, ncols=len(D_values), figsize=(8, 6), sharex=True, sharey=True
 )
-initial_temperatures = np.linspace(200, 400, 40)
+initial_temperatures = np.linspace(150, 600, 200)
 for i, ax in enumerate(axs):
     ebm.D = D_values[i]
     # do some dirty hacks to get the continuation to go faster
-    # stepsize = stepsizes[i]
-    # maxContinuations = maxContinuations_l[i]
-    # tune_factor = tuning_factors[i]
+    stepsize = stepsizes[i]
+    maxContinuations = maxContinuations_l[i]
+    tune_factor = tuning_factors[i]
     for T0 in initial_temperatures:
         print(f"Running continuation for D = {ebm.D:.3f} and T0 = {T0:1f}...")
         # initial guess
         ebm.mu = parameter_range[0]
-        if i == 2 and T0 > 280:
-            ebm.mu = previousMu
+        # if i == 2 and T0 > 280:
+        #     ebm.mu = previousMu
         ebm.T_coeffs = np.zeros(n_polys)
         ebm.T_coeffs[0] = T0
 
@@ -101,6 +101,46 @@ for i, ax in enumerate(axs):
         ax.set_xlabel(r"$\mu$")
 
     ax.set_title(f"D = {ebm.D:.3f}")
+
+# perform detailed search for D = 30
+print("Performing detailed search for D = 30...")
+ebm.D = 30
+ebm.mu = 30
+stepsize = 0.01
+maxContinuations = 10000
+tune_factor = 0.00001
+initial_temperatures = np.linspace(280, 500, 200)
+for T0 in initial_temperatures:
+    print(f"Running continuation for D = {ebm.D:.3f} and T0 = {T0:1f}...")
+    # initial guess
+    ebm.T_coeffs = np.zeros(n_polys)
+    ebm.T_coeffs[0] = T0
+
+    # find initial solution
+    rootfinding.newtonRaphson(ebm)
+
+    # perform continuation
+    T_avgs, mus, stableBranch = continuation.arclengthLoop(
+        ebm,
+        parameter_name,
+        stepsize,
+        tune_factor,
+        parameter_range,
+        maxContinuations,
+    )
+
+    # plot results
+    mus = np.array(mus)
+    T_avgs = np.array(T_avgs)
+    stableBranch = np.array(stableBranch)
+    if np.any(T_avgs < 0):
+        print("Negative temperatures encountered.")
+        continue
+    else:
+        axs[2].plot(mus[stableBranch], T_avgs[stableBranch], "g-")
+        axs[2].plot(mus[~stableBranch], T_avgs[~stableBranch], "r--")
+
+axs[2].set_ylim(150, 350)
 
 fig.suptitle("Continuation of the temperature profile")
 fig.tight_layout()
