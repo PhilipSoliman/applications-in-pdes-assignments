e4e512183ee46a9abd64b8b87d09196429bed0f2
Philip Soliman <philipsoliman4133@gmail.com>
2024-04-26 14:29:37 +0200

    first working continuation

--- code/mcm_model_equations.py
+++ code/mcm_model_equations.py
@@ -6,10 +6,32 @@ import sympy as sym
 from helper import pyutils
 from mcm import MCM
 from root_finding import RootFinding
-# from sympy import init_printing
+from continuation import Continuation
 
+############### Setup Non-linear System of Equations #####
 mcm = MCM()
 sys = mcm.constructSystem()
-mcm.printSystem()
+# mcm.printSystem()
 mcm.printDimensionlessSystem()
 mcm.findStationaryPoints()
+stable_points = mcm.getStableStationaryPoints()
+print("Stable stationary points:")
+pprint(stable_points)
+
+############### Continuation parameters ##################
+tolerance = 1e-5  # tolerance
+maxiter = 100  # maximum number of iterations (per continuation)
+continuation = Continuation(tolerance, maxiter)
+continuation.output = True
+parameter_name = "p1"  # parameter to be continued (should correspond to an attribute of the EBM object)
+parameter_range = (0.5, 1)  # range of the parametervalues of interest
+stepsize = 0.1  # 0.1  # stepsize (needs to be small, why?)
+tune_factor = 0.00001  # 0.001 # tune factor (needs to be small, why?)
+maxContinuations = 1000  # maximum number of continuations
+
+############### Continuation (first test) #################
+for stable_point in stable_points:
+    print(f"Continuation starting at {stable_point}")
+    mcm.x = stable_point
+    errors = continuation.arclength(mcm, parameter_name, stepsize, tune_factor)
+
