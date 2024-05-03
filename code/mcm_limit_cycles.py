7a3535a7b01ac7996c25150455a08457464c05b3
Philip Soliman <philipsoliman4133@gmail.com>
2024-05-03 02:38:40 +0200

    generated limit cycles

--- code/mcm_limit_cycles.py
+++ code/mcm_limit_cycles.py
@@ -30,6 +30,13 @@ pyutils.set_style()
 mcm = MCM()
 n = 3  # number of variables
 
+
+def fun(t: float, y: np.ndarray) -> np.ndarray:  # rhs of mcm for integration
+    mcm.x = y
+    f = mcm.evaluate()
+    return f
+
+
 ############### setup continuation object ################
 tolerance = 1e-5  # tolerance
 maxiter = 100  # maximum number of iterations (per continuation)
@@ -52,59 +59,104 @@ p1 = bifurcations["parameter"]
 mcm.p1 = p1
 
 ############## find limit cycle starting from bifurcation point ################
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
 print("\nFinding limit cycle starting from bifurcation point...")
-cycle = continuation.findFirstLimitCycle(mcm)
-print(cycle.y[:, -1])
+cycle = continuation.shootingMethod(mcm, period_guess=18)
+print(cycle)
 
 # extract point on limit cycle
-cycle_point = cycle.y[:n, -1]
-h_0 = cycle.y[n : 2 * n, -1]
-cycle_parameter = cycle.y[2 * n, -1]
-cycle_period = cycle.y[2 * n + 1, -1]
-mcm.p1 = cycle_parameter
-
-
-# rhs of mcm
-def fun(t: float, y: np.ndarray) -> np.ndarray:
-    mcm.x = y
-    f = mcm.evaluate()
-    return f
+cycle_point = cycle[:n,]
+h_0 = cycle[n : 2 * n]
+cycle_parameter = cycle[2 * n]
+cycle_period = cycle[2 * n + 1]
 
 
 # integrate over one period
-t_span = (0, cycle_period)
-t_eval = np.linspace(*t_span, 1000)
+# mcm.x = cycle_point
+# mcm.p1 = cycle_parameter
+# t_span = (0, 8 * cycle_period)
+# t_eval = np.linspace(*t_span, 1000)
 
-sol = solve_ivp(fun, t_span, cycle_point, t_eval=t_eval, vectorized=True)
+# sol = solve_ivp(fun, t_span, cycle_point, t_eval=t_eval, vectorized=True)
 
 # 3d plot of periodic solution using color gradient to rerpesent time
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-k = cycle.y.shape[1]
-colors = plt.cm.viridis(np.linspace(0, 1, k))
-ax.scatter(cycle.y[0], cycle.y[1], cycle.y[2], "o", color=colors)
+
+# k = sol.y.shape[1]
+# colors = plt.cm.viridis(np.linspace(0, 1, k))
+# ax.scatter(sol.y[0], sol.y[1], sol.y[2], "o", color=colors)
+
+# mean = np.mean(sol.y)
+# print(mean)
+
+######################## find next limit cycle ########################
+# print("\nFinding next limit cycle...")
+# mcm.x = cycle_point + 0.1 * h_0
+# mcm.p1 = cycle_parameter
+# cycle = continuation.shootingMethod(mcm, period_guess=cycle_period)
+# print(cycle)
+
+# # extract point on limit cycle
+# cycle_point = cycle[:n]
+# h_0 = cycle[n : 2 * n]
+# cycle_parameter = cycle[2 * n]
+# cycle_period = cycle[2 * n + 1]
+# mcm.p1 = cycle_parameter
+
+# # integrate over one period
+# t_span = (0, cycle_period)
+# t_eval = np.linspace(*t_span, 1000)
+
+# sol = solve_ivp(fun, t_span, cycle_point, t_eval=t_eval, vectorized=True)
+
+# k = sol.y.shape[1]
+# colors = plt.cm.viridis(np.linspace(0, 1, k))
+# ax.scatter(sol.y[0], sol.y[1], sol.y[2], "o", color=colors)
+
+################## continue limit cycles ##################
+# print("\nContinuing limit cycles...")
+solutions = []
+parameters = []
+periods = []
+cycles = []
+while cycle_parameter <= 1:
+    print(f"Continuing limit cycle with parameter {cycle_parameter}")
+    # set initial guess for next limit cycle
+    mcm.x = cycle_point + 0.13 * h_0
+    mcm.p1 = cycle_parameter
+
+    # find next limit cycle
+    cycle = continuation.shootingMethod(mcm, period_guess=cycle_period)
+
+    # extract point on limit cycle
+    cycle_point = cycle[:n,]
+    h_0 = cycle[n : 2 * n]
+    cycle_parameter = cycle[2 * n]
+    cycle_period = cycle[2 * n + 1]
+
+    print(
+        f"found new cycle with period {cycle_period} and parameter {cycle_parameter} at point {cycle_point}"
+    )
+
+    # store solution
+    solutions.append(cycle_point)
+    parameters.append(cycle_parameter)
+    periods.append(cycle_period)
+
+    # integrate over one period
+    t_span = (0, cycle_period)
+    t_eval = np.linspace(*t_span, 1000)
+    sol = solve_ivp(fun, t_span, cycle_point, t_eval=t_eval, vectorized=True)
+    cycles.append(sol.y)
+
+################## plot limit cycles ##################
+for sol in cycles:
+    k = sol.shape[1]
+    colors = plt.cm.viridis(np.linspace(0, 1, k))
+    ax.scatter(sol[0], sol[1], sol[2], "o", color=colors)
 
 ax.set_xlabel("$x_1$")
 ax.set_ylabel("$x_2$")
 ax.set_zlabel("$x_3$")
 
-# plt.show()
-
-######################## find next limit cycle ########################
-print("\nFinding next limit cycle...")
-mcm.x = cycle_point + 0.1 * h_0
-mcm.p1 = cycle_parameter
-cycle = continuation.findNextLimitCycle(mcm)
-print(cycle.y[:, -1])
-
-# extract point on limit cycle
-cycle_point = cycle.y[:n, -1]
-cycle_period = cycle.y[n, -1]
-cycle_parameter = cycle.y[n + 1, -1]
-mcm.p1 = cycle_parameter
-
-# integrate over one period
-t_span = (0, cycle_period)
-t_eval = np.linspace(*t_span, 1000)
-
-sol = solve_ivp(fun, t_span, cycle_point, t_eval=t_eval, vectorized=True)
+plt.show()
