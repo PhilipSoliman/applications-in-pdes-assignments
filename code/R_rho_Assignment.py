3cf2a60c56d6e0c671f2fb68e5eb6ff38add2e91
Philip Soliman <philipsoliman4133@gmail.com>
2024-06-01 23:25:27 +0200

    generated plot for exercise 1

--- code/R_rho_Assignment.py
+++ code/R_rho_Assignment.py
@@ -2,31 +2,45 @@
 Model code for the Rac-Rho model
 Y.M. Dijkstra, 2024
 """
+
 import matplotlib as mpl
-mpl.use('TkAgg')
+
+mpl.use("TkAgg")
 import matplotlib.pyplot as plt
-import plottingtools as st
-from timeintegrator import time_integrator
-from Newton import Newton, Newton_small
 import numpy as np
+import plottingtools as st
+from Newton import Newton, Newton_small
+from timeintegrator import time_integrator
+
 colours = st.configure()
 from copy import copy
+
+### custom code for styling plots and saving figures
+from helper import pyutils
+
+pyutils.add_modules_to_path()
+pyutils.set_style()
+root = pyutils.get_root()
+output_dir = root / "report" / "figures"
 
 """
 ########################################################################################################################
 Functions and Jacobians
 ########################################################################################################################
 """
-def Fun(y, B, params):              ## Functions for Rac and Rho, returns the d/dt
+
+
+def Fun(y, B, params):  ## Functions for Rac and Rho, returns the d/dt
     A, R0, rho0, deltaR, n = params
     R = y[0]
     rho = y[1]
     F = np.zeros(2)
-    F[0] = A/(rho0**n + rho**n)*(1-R)-deltaR*R
-    F[1] = B/(R0**n + R**n)*(1-rho)-rho
+    F[0] = A / (rho0**n + rho**n) * (1 - R) - deltaR * R
+    F[1] = B / (R0**n + R**n) * (1 - rho) - rho
     return F
 
-def Fun_ext(y, params):         ## Extended function with parameter; with F[2] dB/dt.
+
+def Fun_ext(y, params):  ## Extended function with parameter; with F[2] dB/dt.
     A, R0, rho0, deltaR, n, Bmax, eps = params
     F = np.zeros(3)
     B = y[2]
@@ -37,23 +51,26 @@ def Fun_ext(y, params):         ## Extended function with parameter; with F[2] d
     #####
     return F
 
-def Jac(y, B, params):          ## Jacobian
+
+def Jac(y, B, params):  ## Jacobian
     A, R0, rho0, deltaR, n = params
     R = y[0]
     rho = y[1]
-    J = np.zeros((2,2))
-    J[0,0] = -A/(rho0**n+rho**n)-deltaR
-    J[0,1] = -A*(1-R)*n*rho**(n-1)/(rho0**n+rho**n)**2
-    J[1,0] = -B*(1-rho)*n*R**(n-1)/(R0**n+R**n)**2
-    J[1,1] = -B/(R0**n+R**n)-1
+    J = np.zeros((2, 2))
+    J[0, 0] = -A / (rho0**n + rho**n) - deltaR
+    J[0, 1] = -A * (1 - R) * n * rho ** (n - 1) / (rho0**n + rho**n) ** 2
+    J[1, 0] = -B * (1 - rho) * n * R ** (n - 1) / (R0**n + R**n) ** 2
+    J[1, 1] = -B / (R0**n + R**n) - 1
     return J
 
-def Jac_B(y, B, params):        ## Derivative w.r.t. B
+
+def Jac_B(y, B, params):  ## Derivative w.r.t. B
     R = y[0]
     rho = y[1]
     JB = np.zeros(2)
-    JB[1] = (1-rho)/(R0**n+R**n)
+    JB[1] = (1 - rho) / (R0**n + R**n)
     return JB
+
 
 """
 ########################################################################################################################
@@ -66,17 +83,17 @@ Commands for running the scripts
 A = 0.003
 R0 = 0.3
 rho0 = 0.16
-deltaR =1
+deltaR = 1
 n = 4
 ds = 0.001
 B0 = 0.001
 Bmax_bifdiag = 0.1  # maximum value of B for continuation; terminate for larger B
 
-eps = 0.0         # rate of change of B, used in exercise 3, 4
-Bmax = 0.04         # value Bmax, only used in exercise 4
+eps = 0.0  # rate of change of B, used in exercise 3, 4
+Bmax = 0.04  # value Bmax, only used in exercise 4
 
 dt = 0.01
-T = 10
+T = 20
 
 
 # initialise arrays for output
@@ -84,13 +101,13 @@ params = (A, R0, rho0, deltaR, n)
 params_ext = (A, R0, rho0, deltaR, n, Bmax, eps)
 B_arr = []  # solution y
 y_arr = []  # parameter B
-evs = []    # largest eigenvalues
-all_evs = [] # list of all eigenvalues
+evs = []  # largest eigenvalues
+all_evs = []  # list of all eigenvalues
 
 ########################################################################################################################
 ## Find equilibria for variable B=B0
 ########################################################################################################################
-y0 = np.array([1,1])
+y0 = np.array([1, 1])
 B = B0
 y = Newton_small(Fun, Jac, y0, B, params)
 
@@ -107,18 +124,18 @@ all_evs.append((np.real(ev)))
 
 # start continuation
 iter = 0
-while B<Bmax_bifdiag and iter<3000:
+while B < Bmax_bifdiag and iter < 3000:
     # predictor
     J = np.zeros((3, 3))
     rhs = np.zeros(3)
-    J[:2,:2] = Jac(y, B, params)
+    J[:2, :2] = Jac(y, B, params)
     J[:2, -1] = Jac_B(y, B, params)
     J[-1, 1] = 1
     rhs[-1] = 1
-    z = np.linalg.solve(J, rhs)     #z contains components da/ds and dmu/ds
-    z_rescale = z/np.sqrt(np.sum(z**2))   # now scaled s.t. total step length is ds
-    y_pred = y + z_rescale[:-1]*ds
-    B_pred = B + z_rescale[-1]*ds
+    z = np.linalg.solve(J, rhs)  # z contains components da/ds and dmu/ds
+    z_rescale = z / np.sqrt(np.sum(z**2))  # now scaled s.t. total step length is ds
+    y_pred = y + z_rescale[:-1] * ds
+    B_pred = B + z_rescale[-1] * ds
 
     # corrector
     y, B = Newton(Fun, Jac, Jac_B, y_pred, B_pred, y, B, ds, params)
@@ -126,31 +143,33 @@ while B<Bmax_bifdiag and iter<3000:
     # compute eigenvalues for assessing stability
     J_ev = Jac(y, B, params)
     ev = np.linalg.eigvals(J_ev)
-    evs.append(np.max(np.real(ev)))     #save maximum real value of evs
+    evs.append(np.max(np.real(ev)))  # save maximum real value of evs
     all_evs.append((np.real(ev)))
 
     # update the output arrays
     B_arr.append(B)
     y_arr.append(y)
-    iter +=1
+    iter += 1
 
 # convert lists to arrays
-B_arr = np.asarray(B_arr)               # array of values of B
-y_arr = np.asarray(y_arr)               # array with values of y, shape (length(B), 2), where the 2nd dimension has entries Rac, rho
+B_arr = np.asarray(B_arr)  # array of values of B
+y_arr = np.asarray(
+    y_arr
+)  # array with values of y, shape (length(B), 2), where the 2nd dimension has entries Rac, rho
 evs = np.asarray(evs)
 all_evs = np.asarray(all_evs)
-y_s = copy(y_arr)                      # split y into stable ('y_s') and unstable ('y_u')
-y_s[np.where(evs>0)[0],:] = np.nan
+y_s = copy(y_arr)  # split y into stable ('y_s') and unstable ('y_u')
+y_s[np.where(evs > 0)[0], :] = np.nan
 y_u = copy(y_arr)
-y_u[np.where(evs<0)[0],:] = np.nan
+y_u[np.where(evs < 0)[0], :] = np.nan
 evs_s = copy(evs)
-evs_s[np.where(evs>0)] = np.nan
+evs_s[np.where(evs > 0)] = np.nan
 evs_u = copy(evs)
-evs_u[np.where(evs<0)] = np.nan
+evs_u[np.where(evs < 0)] = np.nan
 all_evs_s = copy(all_evs)
-all_evs_s[np.where(evs>0)[0],:] = np.nan
+all_evs_s[np.where(evs > 0)[0], :] = np.nan
 all_evs_u = copy(all_evs)
-all_evs_u[np.where(evs<0)[0],:] = np.nan
+all_evs_u[np.where(evs < 0)[0], :] = np.nan
 
 ########################################################################################################################
 # Time integrator
@@ -160,57 +179,65 @@ params_ext = (A, R0, rho0, deltaR, n, Bmax, eps)
 
 # the time integrator has arguments: function, parameter list (see above), tuple of initial conditions (R, rho, B), time step, end time.
 # y_tim has shape (3, length(t)). In the first dimension it has entries Rac, rho, B
-t,y_tim = time_integrator(Fun_ext, params_ext, (0.1,0.05,0.03), dt, T)
+t, y_tim = time_integrator(Fun_ext, params_ext, (0.1, 0.05, 0.03), dt, T)
 
 ########################################################################################################################
 # Plot
 ########################################################################################################################
 ## Figure 1 ##
-plt.figure(1, figsize=(2,2))
-plt.subplot(2,2,1)
+plt.figure(1, figsize=(2, 2))
+plt.subplot(2, 2, 1)
 # Plot result of continuation Rac vs B
-plt.plot(B_arr, y_s[:,0], '-', color=colours[0])
-plt.plot(B_arr, y_u[:,0], '--', color=colours[0])
+plt.plot(B_arr, y_s[:, 0], "-", color=colours[0])
+plt.plot(B_arr, y_u[:, 0], "--", color=colours[0])
 
 # Plot trajectory in same plot
-plt.plot(y_tim[-1,:], y_tim[0,:], '-', color=colours[1])    # trajectory y_tim[-1,:] = B(t), y_tim[0,:] = Rac(t)
-plt.plot(y_tim[-1,0], y_tim[0,0], 'o', color=colours[1])    # mark initial condition
-plt.xlabel('B')
-plt.ylabel('Rac')
+plt.plot(
+    y_tim[-1, :], y_tim[0, :], "-", color=colours[1]
+)  # trajectory y_tim[-1,:] = B(t), y_tim[0,:] = Rac(t)
+plt.plot(y_tim[-1, 0], y_tim[0, 0], "o", color=colours[1])  # mark initial condition
+plt.xlabel("B")
+plt.ylabel("Rac")
 plt.ylim(0, 1)
 plt.xlim(0, Bmax_bifdiag)
 
-plt.subplot(2,2,2)
+plt.subplot(2, 2, 2)
 # Plot result of continuation rho vs B
-plt.plot(B_arr, y_s[:,1], '-', color=colours[0])
-plt.plot(B_arr, y_u[:,1], '--', color=colours[0])
+plt.plot(B_arr, y_s[:, 1], "-", color=colours[0])
+plt.plot(B_arr, y_u[:, 1], "--", color=colours[0])
 
 # Plot trajectory in same plot
-plt.plot(y_tim[-1,:], y_tim[1,:], '-', color=colours[1])    # trajectory y_tim[-1,:] = B(t), y_tim[0,:] = rho(t)
-plt.plot(y_tim[-1,0], y_tim[1,0], 'o', color=colours[1])    # mark initial condition
-plt.xlabel('B')
-plt.ylabel('rho')
+plt.plot(
+    y_tim[-1, :], y_tim[1, :], "-", color=colours[1]
+)  # trajectory y_tim[-1,:] = B(t), y_tim[0,:] = rho(t)
+plt.plot(y_tim[-1, 0], y_tim[1, 0], "o", color=colours[1])  # mark initial condition
+plt.xlabel("B")
+plt.ylabel("rho")
 plt.ylim(0, 1)
 plt.xlim(0, Bmax_bifdiag)
 
-plt.subplot(2,2,3)
+plt.subplot(2, 2, 3)
 # Plot result of continuation Rac vs rho
-plt.plot(y_s[:,0], y_s[:,1], '-', color=colours[0])
-plt.plot(y_u[:,0], y_u[:,1], '--', color=colours[0])
+plt.plot(y_s[:, 0], y_s[:, 1], "-", color=colours[0])
+plt.plot(y_u[:, 0], y_u[:, 1], "--", color=colours[0])
 
 # Plot trajectory in same plot
-plt.plot(y_tim[0,:], y_tim[1,:], '-', color=colours[1])
-plt.plot(y_tim[0,0], y_tim[1,0], 'o', color=colours[1])
+plt.plot(y_tim[0, :], y_tim[1, :], "-", color=colours[1])
+plt.plot(y_tim[0, 0], y_tim[1, 0], "o", color=colours[1])
 Bplot = 1
-plt.xlabel('Rac')
-plt.ylabel('rho')
+plt.xlabel("Rac")
+plt.ylabel("rho")
 plt.ylim(0, 1)
 plt.xlim(0, 1)
 
-plt.subplot(2,2,4)
+plt.subplot(2, 2, 4)
 # B as function of t
-plt.plot(t, y_tim[2,:], '-', color=colours[1])
-plt.ylabel('B')
-plt.xlabel('t')
+plt.plot(t, y_tim[2, :], "-", color=colours[1])
+plt.ylabel("B")
+plt.xlabel("t")
 
 st.show()
+
+# save figure
+filename = f"cell_biology_R0={R0}_rho0={rho0}_deltaR={deltaR}_n={n}_Bmax={Bmax}_eps={eps}.png"
+plt.savefig(output_dir / filename)
