fixed limit cycle and corresponding stability calculation

code/classes/continuation.py
@@ -140,7 +140,7 @@ class Continuation:
         parameterValues = []
         stableBranch = []
         bifurcationPoints = []
-        eigvals_around_bif = [] # contains eigs just before and after bifurcation
+        eigvals_around_bif = []  # contains eigs just before and after bifurcation
         self.stableBranch = self.checkStability(nls)
         while True:
             solution = nls.get_current_solution()
@@ -214,7 +214,7 @@ class Continuation:
             parameter=np.array(parameterValues),
             stable=np.array(stableBranch),
             bifurcations=bifurcationPoints,
-            eigvals_around_bif=eigvals_around_bif
+            eigvals_around_bif=eigvals_around_bif,
         )
         return solutions
 
@@ -754,35 +754,60 @@ class Continuation:
             print(msg, **kwargs)
 
     def shootingMethod(
-        self, nls: NonLinearSystem, period_guess: float, tolerance=1e-10, maxiter=200
+        self,
+        nls: NonLinearSystem,
+        type: str,  # specify if continuation or switching
+        period_guess: float = None,
+        tolerance=1e-12,
+        maxiter=200,
+        stepsize=None,
     ):
         """
         shooting method for finding limit cycles
         """
+        # get current solution and parameter
         self.nls = nls
         solution = self.nls.get_current_solution()
         parameter = getattr(self.nls, self.parameterName)
+        self.oldParameter = parameter
         self.n = len(solution)
+
+        # objective function for the shooting method
+        if type == "cont":
+            objective = self.shootingMethodObjectiveCont
+            y_0 = np.zeros(self.n + 2)
+            y_0[: self.n] = solution
+            y_0[self.n] = parameter
+            y_0[self.n + 1] = period_guess
+            if stepsize is None:
+                raise ValueError("Stepsize required for periodic branch cont.")
+            self.pcont_stepsize = stepsize
+        elif type == "switch":
+            objective = self.shootingMethodObjectiveSwitch
+            y_0 = np.zeros(2 * self.n + 2)
+            y_0[: self.n] = solution
+            y_0[self.n : self.n * 2] = 0
+            y_0[self.n] = 1
+            y_0[2 * self.n] = parameter  # t=0
+            y_0[2 * self.n + 1] = (
+                period_guess  # 2 * np.pi / self.imaginaryPartHopfEig  # at t=0
+            )
+        elif type == "pdouble":
+            raise NotImplementedError
+            objective = self.shootingMethodObjectivePDouble
+            size = 2 * self.n + 2
+        else:
+            raise ValueError("Type not recognized. Choose 'cont' or 'switch'.")
+
+        # check if period guess is provided
+        if period_guess is None:
+            raise ValueError("Period guess required for switching continuation.")
+
+        # define time span
         self.t_span = (0, 1)
-        y_0 = np.zeros(2 * self.n + 2)
 
-        # initial guess solution at t=0
-        y_0[: self.n] = solution
-        # y_0[: self.n,1] = solution
-
-        # initial guess for h1 at t=0
-        y_0[self.n : self.n * 2] = 1
-
-        # values for parameter
-        y_0[2 * self.n] = parameter  # t=0
-        # y_0[2 * self.n, 1] = parameter  # t=1
-
-        # period guess
-        y_0[2 * self.n + 1] = (
-            period_guess  # 2 * np.pi / self.imaginaryPartHopfEig  # at t=0
-        )
         out, infodict, ier, msg = fsolve(
-            self.shootingMethodObjective,
+            objective,
             y_0,
             xtol=tolerance,
             maxfev=maxiter,
@@ -795,7 +820,7 @@ class Continuation:
             self.print(bcolors.WARNING + msg + bcolors.ENDC)
         return out, ier
 
-    def shootingMethodRHS(self, t: float, y: np.ndarray):
+    def shootingMethodRHSSwitch(self, t: float, y: np.ndarray):
         """
         RHS of the shooting method
         """
@@ -808,10 +833,11 @@ class Continuation:
         f = self.nls.evaluate()
         df = self.nls.evaluate_derivative()
         return np.concatenate(
-            (period * f, period * np.einsum("ij, j -> i", df, h), [0, 0])
+            # (period * f, period * np.einsum("ij, j -> i", df, h), [0, 0])
+            (period * f, period * df @ h, [0, 0])
         )
 
-    def shootingMethodObjective(self, y0):
+    def shootingMethodObjectiveSwitch(self, y0):
         """
         Objective function for the shooting method
         """
@@ -825,7 +851,13 @@ class Continuation:
         df = self.nls.evaluate_derivative()
 
         # integrate
-        sol = solve_ivp(self.shootingMethodRHS, self.t_span, y0, vectorized=False)
+        sol = solve_ivp(
+            self.shootingMethodRHSSwitch,
+            self.t_span,
+            y0,
+            vectorized=False,
+            max_step=0.01,
+        )
 
         # check residuals
         y1 = sol.y
@@ -835,11 +867,61 @@ class Continuation:
         period1 = y1[2 * self.n + 1, -1]
 
         return np.concatenate(
-            (h0 - h1, solution0 - solution1, [h0[0] - 1], [np.sum(h0 * df[0])])
+            (solution0 - solution1, h0 - h1, [np.sum(h0 * df[0]), h0[0] - 1])
+            # (h0 - h1, solution0 - solution1, [np.sum(h0 * df[0]), parameter0 - self.oldParameter - 0.02])
+        )
+
+    def shootingMethodRHSCont(self, t: float, y: np.ndarray):
+        """
+        RHS of the shooting method
+        """
+        solution = y[: self.n]
+        parameter = y[self.n]
+        period = y[self.n + 1]
+        setattr(self.nls, self.parameterName, parameter)
+        self.nls.set_current_solution(solution)
+        f = self.nls.evaluate()
+        # df = self.nls.evaluate_derivative()
+        return np.concatenate((period * f, [0, 0]))
+
+    def shootingMethodObjectiveCont(self, y0):
+        """
+        Objective function for the shooting method
+        """
+        # save initial state
+        solution0 = y0[: self.n]
+        parameter0 = y0[self.n]
+        # period0 = y0[self.n + 1]
+        self.nls.set_current_solution(solution0)
+        setattr(self.nls, self.parameterName, parameter0)
+        f = self.nls.evaluate()
+        # df = self.nls.evaluate_derivative()
+
+        # integrate
+        sol = solve_ivp(
+            self.shootingMethodRHSCont, self.t_span, y0, vectorized=False, max_step=0.005
+        )
+
+        # check residuals
+        y1 = sol.y
+        solution1 = y1[: self.n, -1]
+        parameter1 = y1[self.n, -1]
+        # period1 = y1[self.n + 1, -1]
+
+        return np.concatenate(
+            # (solution0 - solution1, [f[0], parameter0-self.oldParameter-stepsize])
+            (
+                solution0 - solution1,
+                [parameter0 - self.oldParameter - self.pcont_stepsize, f[0]],
+            )
         )
 
     def monodromyMatrix(
-        self, nls, cycle_point: np.ndarray, cycle_parameter: np.ndarray, cycle_period: float
+        self,
+        nls,
+        cycle_point: np.ndarray,
+        cycle_parameter: np.ndarray,
+        cycle_period: float,
     ) -> np.ndarray:
         """
         Calculate the monodromy matrix of a limit cycle.
@@ -857,7 +939,14 @@ class Continuation:
             y_0[self.n + j] = 1
             y_0[2 * self.n] = cycle_parameter
             y_0[2 * self.n + 1] = cycle_period
-            sol = solve_ivp(self.shootingMethodRHS, self.t_span, y_0, t_eval=t_eval, vectorized=False)
+            sol = solve_ivp(
+                self.shootingMethodRHSSwitch,
+                self.t_span,
+                y_0,
+                t_eval=t_eval,
+                vectorized=False,
+                max_step=0.001,
+            )
             h = sol.y[self.n : 2 * self.n, -1]
             monodromy[:, j] = h
         return monodromy

code/mcm_limit_cycles.py
@@ -58,12 +58,17 @@ mcm.x = x
 p1 = bifurcations["parameter"]
 mcm.p1 = p1
 
-############## find limit cycle starting from bifurcation point ################
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
 print("\nFinding limit cycle starting from bifurcation point...")
-cycle, cycle_valid = continuation.shootingMethod(mcm, period_guess=17.6)
-print(cycle)
+cycle, cycle_valid = continuation.shootingMethod(
+    mcm, "switch", period_guess=17.6, stepsize=0.0
+)
+cycle_point = cycle[:n]
+h = cycle[n : 2 * n]
+cycle_parameter = cycle[2 * n]
+cycle_period = cycle[2 * n + 1]
+# cycle_point = cycle[:n]
+# cycle_parameter = cycle[n]
+# cycle_period = cycle[n + 1]
 
 solutions = []
 parameters = []
@@ -71,31 +76,44 @@ periods = []
 cycles = []
 eigs = []
 stable = []
-cycle_parameter = 0
-while cycle_parameter <= 1:
-    if cycle_valid == 0:
+it = 0
+delta = 0.1
+stable_tolerance = 1e-3
+print("Cycle plot parameters: ")
+num_cycles = 10 
+print(f"\tnum_cycles: {num_cycles}")
+stepsize = 0.005
+print(f"\tstepsize: {stepsize}")
+num_iterations = int((1 - cycle_parameter)//stepsize)
+print(f"\tnum_iterations: {num_iterations}")  
+cycle_spacing = num_iterations // num_cycles
+print(f"\tcycle_spacing: {cycle_spacing}")
+input("Press enter to generate cycle plot...")
+while cycle_parameter <= 1 and it < num_iterations:
+
+    it += 1
+    if not cycle_valid:
         continue
-    print(f"Continuing limit cycle with parameter {cycle_parameter}")
-    # extract point on limit cycle
-    cycle_point = cycle[:n,]
-    h_0 = cycle[n : 2 * n]
-    cycle_parameter = cycle[2 * n]
-    cycle_period = cycle[2 * n + 1]
 
     # calculate current stability
     monodromy = continuation.monodromyMatrix(
         mcm, cycle_point, cycle_parameter, cycle_period
     )
     eig = np.linalg.eigvals(monodromy)
-    print(f"eigenvalues: {eig}")
-    unityEigIndex = np.abs(eig) == 1
+    # check for approximate unity eigenvalues
+    unityEigIndex = np.isclose(np.abs(eig), 1, atol=stable_tolerance)
     if np.all(np.abs(eig[~unityEigIndex]) < 1):
         stable.append(True)
     else:
         stable.append(False)
     eigs.append(eig.tolist())
     print(
-        f"found new cycle with period {cycle_period} and parameter {cycle_parameter} at point {cycle_point}"
+        f"found cycle:"+
+        f"\n\tT: {cycle_period:.3f}"+
+        f"\n\tp: {cycle_parameter:.3f}"+
+        f"\n\tx: ({cycle_point[0]:.2f}, {cycle_point[1]:.2f}, {cycle_point[2]:.2f})"+
+        f"\n\teigs: l1={eig[0]:.2f}, l2={eig[1]:.2f}, l3={eig[2]:.2f}"
+        f"\n\tstable: {stable[-1]}"
     )
 
     # store solution
@@ -104,35 +122,64 @@ while cycle_parameter <= 1:
     periods.append(cycle_period)
 
     # integrate over one period
-    t_span = (0, cycle_period)
-    t_eval = np.linspace(*t_span, 1000)
-    sol = solve_ivp(fun, t_span, cycle_point, t_eval=t_eval, vectorized=True)
-    cycles.append(sol.y.tolist())
+    if it % cycle_spacing == 0:
+        t_span = (0, cycle_period)
+        t_eval = np.linspace(*t_span, 500)
+        sol = solve_ivp(fun, t_span, cycle_point, t_eval=t_eval, vectorized=True)
+        cycles.append(sol.y.tolist())
 
-    # find new limit cycle (see section 7.6.3)
-    new_cycle_point = cycle_point + 0.11 * h_0 
-    # NEW IDEA: solve equation 7.6 to trace branch and find new limit cycle
-    # only do above update if at a hopf bifurcation!
-    # I may have been using th worng update!
-    new_cycle_parameter = cycle_parameter
-    new_cycle_period = cycle_period
-    mcm.x = new_cycle_point
-    mcm.p1 = new_cycle_parameter
-    
-    cycle, cycle_valid = continuation.shootingMethod(mcm, period_guess=cycle_period)
-    cycle_parameter = cycle[2 * n]
+
+    print(f"Continuing limit cycle with parameter {cycle_parameter}")
+    mcm.x = cycle_point
+    if it == 1:
+        mcm.x += delta * h  # start with a small perturbation
+    mcm.p1 = cycle_parameter
+    cycle, cycle_valid = continuation.shootingMethod(
+        mcm, "cont", period_guess=cycle_period, stepsize=stepsize
+    )
+
+    # set new cycle parameters
+    cycle_point = cycle[:n]
+    cycle_parameter = cycle[n]
+    cycle_period = cycle[n + 1]
+
 
 ################## plot limit cycles ##################
-for cycle in cycles:
-    k = len(cycle[0])
-    colors = plt.cm.viridis(np.linspace(0, 1, k))
-    ax.scatter(cycle[0], cycle[1], cycle[2], "o", color=colors)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+num_cycles = len(cycles)
+cycle_parameters = np.array(parameters)[::cycle_spacing]
+parameter_range = np.max(cycle_parameters) - np.min(cycle_parameters)
+tick_locs = (cycle_parameters-np.min(cycle_parameters))/parameter_range
+colors = plt.cm.viridis(tick_locs)
+for i, cycle in enumerate(cycles):
+    cm = ax.scatter(cycle[0], cycle[1], cycle[2], "o", color=colors[i])
+
+# set angle
+ax.view_init(elev=20, azim=-120)
+
+# set colorbar
+scalarmappaple = plt.cm.ScalarMappable(cmap=plt.cm.viridis)
+cb = fig.colorbar(
+    scalarmappaple,
+    ax=ax,
+    label="$p_1$",
+)
+cb.set_ticks(tick_locs[::2])
+cycle_parameters_text = [f"{p:.2f}" for p in cycle_parameters[::2]]
+cb.set_ticklabels(cycle_parameters_text)
 
 ax.set_xlabel("$x_1$")
 ax.set_ylabel("$x_2$")
 ax.set_zlabel("$x_3$")
 
 plt.show()
+
+# save figure
+filename = "mcm_limit_cycles"
+filepath = output_dir / f"{filename}.png"
+fig.savefig(filepath, bbox_inches="tight", dpi=500)
+
 
 ################### save limit cycles to file ##################
 filename = "mcm_limit_cycles.json"

code/mcm_model_equations.py
@@ -124,7 +124,7 @@ for i, stable_point in enumerate(stable_points):
     for i, bif in enumerate(bifs):
         mean = np.mean(bif["solution"])
         ax.plot(bif["parameter"], mean, "ko", label="bifurcation (Hopf)")
-            # print eigs directly before and after bifurcation
+        # print eigs directly before and after bifurcation
         print("\nEigenvalues around bifurcation:")
         print("\t before: ", eigvals_around_bif[i][0])
         print("\t after: ", eigvals_around_bif[i][1])
@@ -176,9 +176,19 @@ with open(filepath, "r") as f:
 stable = np.array(stable).astype(bool)
 parameters = np.array(parameters)
 solutions = np.array(solutions)
-axs[0].plot(parameters[stable], np.mean(solutions[stable], 1), "gx", label="stable limit cycle")
 axs[0].plot(
-    parameters[~stable], np.mean(solutions[~stable], 1), "rx", label="unstable limit cycle"
+    parameters[stable],
+    np.mean(solutions[stable], 1),
+    "g-",
+    label="stable limit cycle",
+    linewidth=3,
+)
+axs[0].plot(
+    parameters[~stable],
+    np.mean(solutions[~stable], 1),
+    "r--",
+    label="unstable limit cycle",
+    linewidth=3,
 )
 axs[0].set_ylabel("$x$")
 axs[0].set_xlabel("$p_1$")